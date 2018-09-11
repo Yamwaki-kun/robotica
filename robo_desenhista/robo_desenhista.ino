f0e0c816517d3eacb46eafef231d7b3074eb2ed6
arnaldo <arnaldoalves34@gmail.com>
2018-09-11 17:10:39 -0300

    modifiquei o arquivo configurei o setup

--- robo_desenhista/robo_desenhista.ino
+++ robo_desenhista/robo_desenhista.ino
@@ -4,11 +4,14 @@
  * 11/09/2018
  */
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(5, OUTPUT)
+  pinMode(6, OUTPUT)
+  pinMode(9, OUTPUT)
+  pinMode(10, OUTPUT)
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
